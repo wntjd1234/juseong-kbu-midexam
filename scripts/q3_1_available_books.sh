966e113ff43e0187309253592ed37cca39dd0e7e
윤주성 <wntjd010616@gmail.com>
2025-05-02 10:15:39 +0900

    [Q3-1] feat: q3_1_available_books.sh 작성

--- scripts/q3_1_available_books.sh
+++ scripts/q3_1_available_books.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jq '.[] | select(.available == true and .copies >= 2) | .title' scripts/library.json
+jq '.[] | select(.available == true and .copies >= 2) | "\(.title) by \(.author)"' scripts/library.json
